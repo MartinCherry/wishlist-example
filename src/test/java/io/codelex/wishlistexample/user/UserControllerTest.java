0f5ffaf094ebce9417028ec837f1815e32e2f757
MartinCherry <martins.kirsis@gmail.com>
2022-11-24 14:59:46 +0200

    Reformatted text

--- src/test/java/io/codelex/wishlistexample/user/UserControllerTest.java
+++ src/test/java/io/codelex/wishlistexample/user/UserControllerTest.java
@@ -1,1 +1,38 @@
-package io.codelex.wishlistexample.user;import io.codelex.wishlistexample.user.domain.User;import io.codelex.wishlistexample.user.domain.UserRequest;import org.junit.jupiter.api.Assertions;import org.junit.jupiter.api.Test;import org.junit.jupiter.api.extension.ExtendWith;import org.mockito.InjectMocks;import org.mockito.Mock;import org.mockito.junit.jupiter.MockitoExtension;import java.util.ArrayList;import java.util.List;public class UserControllerTest {    UserService service = new UserService();    @Test    public void shouldReturnUsers() {        String expectedResult = "Josh, Josh, Josh, Josh, Josh";        UserRequest testUserRequest = createUserRequest();        String result = service.createUsers(testUserRequest);        Assertions.assertEquals(expectedResult, result);    };    private UserRequest createUserRequest() {        User testUser = new User("User", 1, "Josh", "josh@inbox.lv");        List<User> testUserList = new ArrayList<>();        for (int i = 0; i<5; i++) {            testUserList.add(testUser);        }        return new UserRequest(testUserList);    }}
+package io.codelex.wishlistexample.user;
+
+import io.codelex.wishlistexample.user.domain.User;
+import io.codelex.wishlistexample.user.domain.UserRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserControllerTest {
+
+
+    UserService service = new UserService();
+
+
+    @Test
+    public void shouldReturnUsers() {
+        String expectedResult = "Josh, Josh, Josh, Josh, Josh";
+        UserRequest testUserRequest = createUserRequest();
+        String result = service.createUsers(testUserRequest);
+        Assertions.assertEquals(expectedResult, result);
+    };
+
+    private UserRequest createUserRequest() {
+        User testUser = new User("User", 1, "Josh", "josh@inbox.lv");
+        List<User> testUserList = new ArrayList<>();
+        for (int i = 0; i<5; i++) {
+            testUserList.add(testUser);
+        }
+        return new UserRequest(testUserList);
+    }
+
+}
--- src/test/java/io/codelex/wishlistexample/wishes/WishControllerTest.java
+++ src/test/java/io/codelex/wishlistexample/wishes/WishControllerTest.java
@@ -1,1 +1,45 @@
-package io.codelex.wishlistexample.wishes;import io.codelex.wishlistexample.wishes.domain.Wish;import org.junit.jupiter.api.Test;import org.junit.jupiter.api.extension.ExtendWith;import org.mockito.InjectMocks;import org.mockito.Mock;import org.mockito.junit.jupiter.MockitoExtension;import static org.mockito.Mockito.verify;@ExtendWith(MockitoExtension.class)public class WishControllerTest {    Wish testWish = new Wish();    @Mock    WishService service;    @InjectMocks    WishController controller;    @Test    public void shouldInteractWishSaveOrUpdate(){        controller.saveWish(testWish);        verify(service).saveOrUpdate(testWish);    }    @Test    public void shouldInteractWithGetWishById(){        controller.getWIsh(1);        verify(service).getWishById(1);    }    @Test    public void shouldInteractWithGetAllWishes(){        controller.getAllWishes();        verify(service).getAllWishes();    }    @Test    public void shouldInteractWithDeleteWish(){        controller.deleteWish(1);        verify(service).delete(1);    }}
+package io.codelex.wishlistexample.wishes;
+
+import io.codelex.wishlistexample.wishes.domain.Wish;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class WishControllerTest {
+
+    Wish testWish = new Wish();
+    @Mock
+    WishService service;
+
+    @InjectMocks
+    WishController controller;
+
+    @Test
+    public void shouldInteractWishSaveOrUpdate(){
+        controller.saveWish(testWish);
+        verify(service).saveOrUpdate(testWish);
+    }
+
+    @Test
+    public void shouldInteractWithGetWishById(){
+        controller.getWIsh(1);
+        verify(service).getWishById(1);
+    }
+
+    @Test
+    public void shouldInteractWithGetAllWishes(){
+        controller.getAllWishes();
+        verify(service).getAllWishes();
+    }
+
+    @Test
+    public void shouldInteractWithDeleteWish(){
+        controller.deleteWish(1);
+        verify(service).delete(1);
+    }
+
+}
